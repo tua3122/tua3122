fc69dbd01c10081d349056cb17a3975d8b302c2f
Allen Tu <allentu3@gmail.com>
2018-02-06 08:25:21 -0800

    Unit 0

---
+++ Unit 0/src/ReviewQuestions.java
@@ -0,0 +1,28 @@
+
+public class ReviewQuestions {
+	public static void main (String [] args){
+		System.out.println("|\\---/|");
+		System.out.println("| o_o |");
+		System.out.println(" \\_^_/");
+	}
+	
+	public static void One(){
+		System.out.println("------------");
+		System.out.println("| Allen Tu |");
+		System.out.println("------------");
+	}
+	
+	public static void Two(){
+		System.out.println("|\\---/|");
+		System.out.println("| o_o |");
+		System.out.println(" \\_^_/");
+	}
+	
+	public static void Three(){
+		System.out.println("******==========");
+		System.out.println("******==========");
+		System.out.println("******==========");
+		System.out.println("================");
+		System.out.println("================");
+	}
+}
