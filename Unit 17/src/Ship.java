01465f9ed5c7380712474a42541a3b22620fc899
Allen Tu <allentu3@gmail.com>
2018-04-16 09:25:50 -0700

    Unit 17

---
+++ Unit 17/src/Ship.java
@@ -0,0 +1,63 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import javax.imageio.ImageIO;
+
+@SuppressWarnings("unused")
+public class Ship extends MovingThing
+{
+	private int speed;
+	private Image image;
+
+	public Ship()
+	{
+		this(0,0,0);
+	}
+
+	public Ship(int x, int y)
+	{
+		this(x,y,0);
+	}
+
+	public Ship(int x, int y, int s)
+	{
+		super(x, y);
+		speed=s;
+		try
+		{
+			image = ImageIO.read(new File("H:\\tua3122\\Unit 17\\src\\ship.jpg"));
+		}
+		catch(Exception e)
+		{
+			System.out.println("Broken");
+		}
+	}
+
+
+	public void setSpeed(int s)
+	{
+	   speed = s;
+	}
+
+	public int getSpeed()
+	{
+	   return speed;
+	}
+
+	public void draw( Graphics window )
+	{
+		window.drawImage(image,getX(),getY(),80,80,null);
+	}
+
+	public String toString()
+	{
+		return super.toString() + getSpeed();
+	}
+}
